0050889dd97f931a38fc21b132fa31c1fdab1835
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-01 15:23:41 -0300

    feat: create service user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    private users: User[] = []
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        // Foi adicionado esses dois pontos -> : após o parâmetro pq indica o que é que eu quero que essa função retorne, nesse caso quero retornar o User que
+        // está na interface, porém não sei se isso vai retornar de fato, então o JS e o nest reconhece isso com uma Promise e pede pra usar
+        // dessa forma: Promise<User> e assim para de apontar o erro dado de sintaxe
+        return {
+            ...createUserDto,
+            id: 12
+        }
+    }
+}
