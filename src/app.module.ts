2d0e46f4b33241ed7cb2038baabeb7003848e29f
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-02 13:42:44 -0300

    feat: adding configMoudule .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    })
+  ],
   controllers: [],
   providers: [],
 })
