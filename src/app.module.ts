5b1c22badbb89859c77486a27541ac6cccdada8d
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-02 19:56:55 -0300

    feat: config migrations in app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,10 +27,30 @@ import { TypeOrmModule } from '@nestjs/typeorm'
       password: process.env.DB_PASSWORD,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
-      entities: [`${__dirname}/**/*.entity{.js,.ts}`]
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true
     })
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+/**
+ * Explicações sobre a busca de pastas acima em dirname
+ * 
+ * entities: [`${__dirname}/ * * / *.entity{.js,.ts}`],
+ * migrations: [`${__dirname} / migration{.js, * .ts}`] qual a diferença da forma q está buscando esses arquivos? ex: /* * / *.entity qquer dizer q foi voltada uma pasta? 
+ * e o /migration indica q eles estão no mesmo diretório? e pq tem esse astericos no meio {.js,*.ts}
+ * 
+ * Na configuração fornecida, há diferenças na forma como os arquivos são buscados:
+
+entities: [${__dirname}/* * /*.entity{.js,.ts}]: Aqui, a busca está sendo feita recursivamente em todos os diretórios a partir do diretório atual (__dirname). A parte /** /*.entity significa que está procurando por arquivos cujos nomes terminam com .entity. A expressão {.js,.ts} indica que ele está procurando por arquivos com extensão .js ou .ts.
+
+migrations: [${__dirname}/migration{.js,*.ts}]: Nesse caso, a busca está sendo feita apenas no diretório atual (__dirname). A parte /migration indica que está procurando por arquivos específicos dentro desse diretório chamado "migration". A expressão {.js,*.ts} indica que ele está procurando por arquivos com extensão .js ou .ts.
+
+Portanto, a diferença é que a busca de entidades (entities) está sendo feita recursivamente em todos os diretórios a partir do diretório atual, enquanto a busca de migrações (migrations) está sendo feita apenas no diretório atual. Além disso, a expressão {.js,*.ts} é usada para especificar as extensões dos arquivos que estão sendo buscados.
+
+O uso de * como um curinga é uma forma de fazer casamento de padrões nos nomes dos arquivos. No exemplo fornecido, {.js,*.ts} significa que está procurando por arquivos com extensão .js ou qualquer arquivo com extensão .ts. Isso permite que você procure por diferentes tipos de arquivos usando um único padrão.
+ */
