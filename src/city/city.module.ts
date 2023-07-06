4c921d8cd123c2f1bb4554f618b577db5cdeabf8
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-06 16:50:27 -0300

    feat: registry cache in city module

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,10 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CityEntity])],
+  imports: [CacheModule.register({
+    ttl: 900000,
+  }), 
+  TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
   providers: [CityService]
 })
 export class CityModule {}
+
+// forneça ttl em milissegundos pois estamos usando o cahe v5
