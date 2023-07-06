45d326774db10c24778a1be038e6a03c35e96b97
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-06 18:16:14 -0300

    ferat: fecthing global cache

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -2,34 +2,17 @@ import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CityEntity } from './entities/city.entity';
-import { Cache } from 'cache-manager';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
 export class CityService {
-
     constructor(
         @InjectRepository(CityEntity)
         private readonly cityRepository: Repository<CityEntity>,
-        @Inject(CACHE_MANAGER) private cacheManager: Cache
+        private readonly cacheService: CacheService
      ) {}
 
     async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-        const citiesCache: CityEntity[] = await this.cacheManager.get(`${stateId}`);
-
-        if(citiesCache) {
-            console.log('citiesCache');
-            return citiesCache
-        }
-
-        const cities = await this.cityRepository.find({
-            where: {
-                stateId
-            }
-        })
-
-        await this.cacheManager.set( `${stateId}`, cities )
-
-        return cities
+        return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () => this.cityRepository.find({ where: {stateId} }))
     }
 }
