b630b5a5400010d3ae3487cb457b0f131def552f
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-06 16:50:01 -0300

    feat: add cache in citiy

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,21 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CityEntity } from './entities/city.entity';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class CityService {
 
     constructor(
         @InjectRepository(CityEntity)
-        private readonly cityRepository: Repository<CityEntity>
+        private readonly cityRepository: Repository<CityEntity>,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
      ) {}
 
-    async getAllCitiesByStateId(stateIdAux: number): Promise<CityEntity[]> {
-        return this.cityRepository.find({
+    async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
+        const citiesCache: CityEntity[] = await this.cacheManager.get(`${stateId}`);
+
+        if(citiesCache) {
+            console.log('citiesCache');
+            return citiesCache
+        }
+
+        const cities = await this.cityRepository.find({
             where: {
-                stateIdAux
+                stateId
             }
         })
+
+        await this.cacheManager.set( `${stateId}`, cities )
+
+        return cities
     }
 }
