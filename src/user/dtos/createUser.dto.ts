757320831665572d17f56affda118d172368fe55
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-08 20:20:43 -0300

    feat: add class validators to CreateUserDto

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,7 +1,18 @@
-export interface CreateUserDto {
-    name: string,
-    email: string,
-    phone: string,
-    cpf: string,
+import { IsString } from "class-validator"
+
+export class CreateUserDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    email: string;
+
+    @IsString()
+    phone: string;
+
+    @IsString()
+    cpf: string;
+    
+    @IsString()
     password: string
 }
