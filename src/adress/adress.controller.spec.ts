099b8dab7ec21866a13d938556dfad5a73b9a54b
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-10 14:22:54 -0300

    test: controller adress

---
+++ src/adress/adress.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AdressController } from './adress.controller';
+
+describe('AdressController', () => {
+  let controller: AdressController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AdressController],
+    }).compile();
+
+    controller = module.get<AdressController>(AdressController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
