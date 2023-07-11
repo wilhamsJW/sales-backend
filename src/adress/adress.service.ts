79c60e1c6361f9210ef7641bcb76c3fdb9e2cf3a
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-10 21:43:21 -0300

    feat: add function search user and city validation

--- src/adress/adress.service.ts
+++ src/adress/adress.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CityService } from 'src/city/city.service';
+import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { CreateAddressDto } from './dtos/createAddres.dto';
 import { AdressEntity } from './entities/adress.entity';
@@ -10,9 +12,13 @@ export class AdressService {
     constructor(
         @InjectRepository(AdressEntity)
         private readonly addressRepository: Repository<AdressEntity>,
+        private readonly userService: UserService,
+        private readonly cityService: CityService
     ) {}
 
     async createAddress(createAddressDto: CreateAddressDto, userId: number): Promise<AdressEntity> {
+        await this.userService.findUserByID(userId)
+        await this.cityService.findCityById(createAddressDto.cityId)
         return this.addressRepository.save({
             ...createAddressDto,
             userId,
