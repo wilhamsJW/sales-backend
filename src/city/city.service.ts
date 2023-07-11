f6728c71b3726ce36cd1f764ca965b9d9f79be90
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-10 21:41:58 -0300

    feat: implemented for search city and validation

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CityEntity } from './entities/city.entity';
 import { CacheService } from 'src/cache/cache.service';
+import { NotFoundException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class CityService {
@@ -14,5 +15,18 @@ export class CityService {
 
     async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
         return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () => this.cityRepository.find({ where: {stateId} }))
+    }
+
+    async findCityById(cityId: number): Promise<CityEntity> {
+        const city = await this.cityRepository.findOne({
+            where: {
+                id: cityId
+            }
+        });
+
+        if (!city) {
+            throw new NotFoundException(`CityId: ${cityId} not found.`)
+        }
+        return city;
     }
 }
