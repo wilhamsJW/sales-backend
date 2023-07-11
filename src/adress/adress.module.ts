da91dc25ef9c61192e8474898bc19dcebbf3719f
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-10 21:43:43 -0300

    feat: add imports mdule address

--- src/adress/adress.module.ts
+++ src/adress/adress.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityModule } from 'src/city/city.module';
+import { UserModule } from 'src/user/user.module';
 import { AdressController } from './adress.controller';
 import { AdressService } from './adress.service';
 import { AdressEntity } from './entities/adress.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdressEntity])],
+  imports: [TypeOrmModule.forFeature([AdressEntity]), UserModule, CityModule],
   controllers: [AdressController],
   providers: [AdressService]
 })
