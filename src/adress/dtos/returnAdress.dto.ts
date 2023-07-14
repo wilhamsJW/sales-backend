89066562cf4c536436372b7f9f8cacac1b57ac54
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-13 22:08:25 -0300

    feat: add returnCityDto in class ReturnAddressDto

--- src/adress/dtos/returnAdress.dto.ts
+++ src/adress/dtos/returnAdress.dto.ts
@@ -1,13 +1,20 @@
+import { ReturnCityDto } from "src/city/dtos/returnCity.dto";
 import { AdressEntity } from "../entities/adress.entity";
 
 export class ReturnAddressDto {
     complement: string;
     numberAndress: number;
     cep: string;
+    city?: ReturnCityDto
 
     constructor(address: AdressEntity) {
         this.complement = address.complement
         this.numberAndress = address.numberAndress
         this.cep = address.cep
+        this.city = address.city ? new ReturnCityDto(address.city) : null
     }
 }
+/**
+ * new ReturnCityDto(address.city) = estou enviando os dados para esta class chamada ReturnCityDto
+ * ela funciona como um formatador de dados e ela é um DTO ou seja um Data Trasnferer, está transferindo meus dados
+ */
