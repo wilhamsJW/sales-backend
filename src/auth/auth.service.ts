62cf279aef160ff438b952577c2b8fe5c14bf066
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-14 22:17:54 -0300

    feat: returned accessToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,13 +4,17 @@ import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dtos/login.dto';
 import { NotFoundException } from '@nestjs/common/exceptions';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt'
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService, private jwtService: JwtService) {}
 
-    async login(loginDto: LoginDto): Promise<UserEntity> {
+    async login(loginDto: LoginDto): Promise<ReturnLogin> {
         const user: UserEntity | undefined = await this.userService
         .findUserByEmail(loginDto.email)
         .catch(() => undefined);
@@ -21,6 +25,9 @@ export class AuthService {
             throw new NotFoundException('Email or password invalid');
         }
 
-        return user;
+        return {
+            accessToken: this.jwtService.sign({ ...new LoginPayload(user) }), // jwtService.sign - responsável por gerar o token JWT
+            user: new ReturnUserDto(user)
+        };
     }
 }
