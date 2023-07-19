5ebc40934dd8863d0f650032fc50f29b9cf0cc0e
wilhams junior <wilhams.junior@mundiale.com.br>
2023-07-19 10:48:42 -0300

    feat: create file for user identification

---
+++ src/user/enum/user.type.num.ts
@@ -0,0 +1,10 @@
+export enum UserType {
+    User = 1,
+    Admin = 2,
+    Moderator = 3
+  }
+
+  /**
+   * Os arquivos Enum do Nest.js são uma prática comum para melhorar a legibilidade e a manutenibilidade do código, 
+   * especialmente quando há um conjunto específico de valores que precisam ser usados consistentemente em todo o aplicativo.
+   */
